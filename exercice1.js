545f32f4f05972290392a7724dba105added83e2
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-04 10:19:47 +0100

    jhyrtgerf

--- exercice1.js
+++ exercice1.js
@@ -1,4 +1,14 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML= "Ce div a été ajouté en utilisant javascript";
+monDiv.style.backgroundColor = "red";
+monDiv.style.width = "100px";
+monDiv.style.height = "100px";
+
+let divView = document.createElement('div');
+document.getElementById('viewport').appendChild(divView);
+
+let supp = document.getElementById("aSupprimer");
+supp.remove();
 
 document.getElementById('viewport').appendChild(monDiv);
 
